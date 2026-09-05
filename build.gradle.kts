17164a9331528170ec78a293261469219c9b3461
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-09-06 01:08:21 +0200

    Update file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,9 @@
 plugins {
-    // Регистрация плагинов без применения к корню (apply false).
-    // Модуль :app активирует их через алиасы из каталога libs.versions.toml.
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.serialization) apply false
 }
 
-// Задача очистки сборочных артефактов по современному API Gradle 9.x
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }

600cf02dd332b0a728c29c5d673745efe1c34216
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-09-06 06:35:18 +0200

    [Pipeline] ChatGptScreen.kt

--- app/src/main/kotlin/com/clientg/ChatGptScreen.kt
+++ app/src/main/kotlin/com/clientg/ChatGptScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
